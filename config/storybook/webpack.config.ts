090cf05232688126ac19978544526469d18d6fb3
sxkzxqw <marsel.vagizov@bk.ru>
2023-05-29 22:57:16 +0300

    ts fix

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -14,6 +14,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
     config.resolve?.extensions?.push('.ts', '.tsx');
 
     // eslint-disable-next-line no-param-reassign
+    // @ts-ignore : Unreachable code error
     config?.module?.rules = config?.module?.rules?.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
