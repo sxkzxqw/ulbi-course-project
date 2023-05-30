88e71732b2c2ba063631c3c4883eeb16077c7bee
sxkzxqw <marsel.vagizov@bk.ru>
2023-05-30 15:31:43 +0300

    ci pipeline fix

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -3,19 +3,18 @@ import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
 
-export default ({ config }: { config: webpack.Configuration }) => {
+export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
         build: '',
         html: '',
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
-    config?.resolve?.modules?.push(paths.src);
-    config.resolve?.extensions?.push('.ts', '.tsx');
+    config.resolve.modules.push(paths.src);
+    config.resolve.extensions.push('.ts', '.tsx');
 
     // eslint-disable-next-line no-param-reassign
-    // @ts-ignore : Unreachable code error
-    config?.module?.rules = config?.module?.rules?.map((rule: RuleSetRule) => {
+    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }
@@ -23,11 +22,11 @@ export default ({ config }: { config: webpack.Configuration }) => {
         return rule;
     });
 
-    config.module?.rules?.push({
+    config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
-    config.module?.rules?.push(buildCssLoader(true));
+    config.module.rules.push(buildCssLoader(true));
 
     return config;
 };
